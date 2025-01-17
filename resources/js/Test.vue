fad512d5b5289d5b805d9aa458086ff77e9cd013
Todd Mannherz <todd@reelivate.com>
2025-01-17 16:05:02 -0500

    initial add

---
+++ resources/js/Test.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>Test component 1</p>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('');
+</script>
---
+++ resources/js/Test2.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>Test component 2</p>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('');
+</script>
